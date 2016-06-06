11ea1163a7ca67e459bf68bf593333eecf2cfa8e
Skif <v.chigikov@yandex.ru>
2016-06-06 15:17:07 +0300

    init

---
+++ my-addons/my-addons/my-theme-01/__openerp__.py
@@ -0,0 +1,11 @@
+{
+  'name':'Skif theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Skif',
+  'data': [ 'views/layout.xml' ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_blog', 'sale'],
+  'installable': True,
+  'application': True,
+}
